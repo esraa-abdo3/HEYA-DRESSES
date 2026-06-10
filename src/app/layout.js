179236b1b5b6e0540da6b62348bd87cf14fb5e40
esraa-abdo3 <esraaabdalnasserzz@gmail.com>
2026-06-11 02:46:34 +0300

    test pixel

--- src/app/layout.js
+++ src/app/layout.js
@@ -66,6 +66,7 @@ import { getAllpromocodes } from "./Context/Promocodeserver";
 import GuestInit from "./prodivders/gestprovider";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import Script from "next/script";
 
 export default async function RootLayout({ children }) {
   const cart = await getInitialCart();
@@ -81,6 +82,34 @@ const cleanpromocodes= JSON.parse(JSON.stringify(promocodes));
   return (
     <html lang="en">
       <body>
+      
+              <Script
+        id="meta-pixel-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${PIXEL_ID}');
+          `,
+        }}
+      />
+      <noscript>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
         <AuthProvider>
           <GuestInit isGuest={isGuest} />
 
